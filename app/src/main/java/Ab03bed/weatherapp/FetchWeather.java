98483e5548bb2fa7d6dbd14d0762379524113855
ybond93 <y.bahem@gmail.com>
2024-02-20 01:32:42 +0100

    message to abbe

--- app/src/main/java/Ab03bed/weatherapp/FetchWeather.java
+++ app/src/main/java/Ab03bed/weatherapp/FetchWeather.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface FetchWeather {
-
+    //why you leave alone my darling, dont you have a heart in you? i feel so lonly when you go to your famliy and donot care about the bread and the salt between us? signed by #doyouwantacarAbro
     //@GET("/weatherapi/locationforecast/2.0/classic?")
     //Call<Weather> getWeatherData(@Query("lat") String lat, @Query("lon") String lon);
 
